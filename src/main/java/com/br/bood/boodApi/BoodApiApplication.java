f56e6ff3222dfd59feacb3549e29c27e54e7ed0f
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-03-07 11:01:20 -0300

    feat: spring initializr

---
+++ src/main/java/com/br/bood/boodApi/BoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.bood.boodApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/bood/boodApi/BoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bood.boodApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
