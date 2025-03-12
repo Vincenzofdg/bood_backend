46f2a6fdade5ed19ac33cb16add22236ef8698f8
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-03-12 17:50:14 -0300

    fix: spring boot test

--- src/test/java/com/br/bood/boodApi/BoodApiApplicationTests.java
+++ src/test/java/com/br/bood/boodApi/BoodApiApplicationTests.java
@@ -1,13 +1,12 @@
 package com.br.bood.boodApi;
 
+import com.br.bood.boodApi.application.BoodApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BoodApiApplication.class)
 class BoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
